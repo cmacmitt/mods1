2b1860b094ee0a1aeea8fc0f0c6d0392bd0bed69
Chris MacDonald <chris.macdonald@mitt.ca>
2023-02-22 14:14:31 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// import { default as bob } from "./utils.js"; // default
+// import { sayWhat as saySomething } from "./utils.js"; // named
+
+import * as ted from "./utils.js";
+
+// ted.default = "some other value";
+console.log(ted);
+
+// console.log(bob);
+// saySomething();
---
+++ js/utils.js
@@ -0,0 +1,15 @@
+const someValue = "My Value";
+
+export default someValue;
+
+export const anotherValue = 10;
+export let someLetValue = 20;
+
+// someValue = "another value";
+function sayWhat() {
+  console.log("What?!");
+}
+
+// export default someValue;
+// export default sayWhat;
+// export { sayWhat };
